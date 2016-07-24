5509de38d804fc3fbebc17ece2e13067e89ee5b3
Oleg <js7ussr@gmail.com>
2016-07-24 18:02:40 +0500

    -Хранение пока так

--- src/com/company/MySearcher.java
+++ src/com/company/MySearcher.java
@@ -1,16 +1,34 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.HashMap;
 
 /**
  * Created by tailer_d on 23.07.2016.
  */
 public class MySearcher implements ISearcher {
+    String[] innerClassNames;
+    HashMap<String, Long> hashMapClasses;
 
 
     @Override
     public void refresh(String[] classNames, long[] modificationDates) {
+        int lenghtOfClassNames=classNames.length;
+        long startTime=System.currentTimeMillis();
 
+        hashMapClasses=new HashMap<>(lenghtOfClassNames);
+        innerClassNames = classNames;
+
+        for(int i=0;i<lenghtOfClassNames;i++){
+            hashMapClasses.put(classNames[i],modificationDates[i]);
+        }
+
+        Arrays.sort(classNames);
+
+        long endTime=System.currentTimeMillis()-startTime;
+        System.out.println("Время создания HashMap:"+endTime);
     }
 
     @Override
